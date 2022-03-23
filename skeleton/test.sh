6952c6708147a1e5a95091b8620312f9b9f2a48d
Remy GENY <rgeny@e2r2p11.clusters.42paris.fr>
2022-03-23 14:13:18 +0100

    génération automatique script tester

---
+++ skeleton/test.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+###############################################
+##################### OPT #####################
+###############################################
+
+OPT_LIST=$*
+
+function get_opt()
+{
+	if [ "$OPT_LIST" == "" ] || [ "$*" == "" ]
+	then
+		return 0
+	fi
+	for i in $OPT_LIST
+	do
+		for j in $*
+		do
+			if [ "$j" == "$i" ]
+			then
+				return 1
+			fi
+		done
+	done
+	return 0
+}
+
+get_opt "-h" "--help"
+if [ "$?" == "1" ]
+then
+	echo "Options :"
+	echo "	--help (-h)"
+	echo "	--verbose (-v)"
+	echo "	--stop (-s)"
+	echo "tests options :"
+	echo "	vector"
+	echo "	map"
+	echo "	stack"
+	exit
+fi
+
+get_opt	"-v" "--verbose"
+VERBOSE="$?"
+
+get_opt "-s" "--stop"
+STOP="$?"
+
+###############################################
+#################### TEST #####################
+###############################################
+
+
+
