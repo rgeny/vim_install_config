a1d687704a04554a3dbb414cf3e968282259cadb
Remy GENY <rgeny@e2r2p12.clusters.42paris.fr>
2022-03-25 12:58:42 +0100

    modif skeleton test.sh

--- skeleton/test.sh
+++ skeleton/test.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 
 ###############################################
-##################### LOGS ####################
+##################### VARS ####################
 ###############################################
 
-LOGS_DIR=log/
+TIMEOUT="timeout 2s"
+OPT_LIST=$*
+SCRIPT=$(basename "$0")
+
+TESTS_DIR=tests/
+
+LOGS_DIR=logs/
 mkdir -p $LOGS_DIR
-CUR_LOGS_DIR=$LOGS_DIR$(data +"%F_%T")
+CUR_LOGS_DIR=$LOGS_DIR$(date +"%F_%T")
+find $CUR_LOGS_DIR 2>/dev/null 1>/dev/null
+if [ "$?" == "0" ]
+then
+	printf "Duplicated log directory, wait 1 sec\n"
+	sleep 1
+	CUR_LOGS_DIR=$LOGS_DIR$(date +"%F_%T")
+fi
 mkdir $CUR_LOGS_DIR
+LOG_RESULT="result"
+LOG_RESEARCH="research"
+LOG_TEST="test"
+
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+PURPLE="\033[0;35m"
+CYAN="\033[0;36m"
+WHITE="\033[0;37m"
 
 
 ###############################################
 ##################### OPT #####################
 ###############################################
 
-OPT_LIST=$*
 
 function get_opt()
 {
@@ -47,6 +70,10 @@ then
 	echo "	--verbose (-v)"
 	echo "	--stop (-s)"
 	echo "	--logclean (-lc)"
+	echo "	--clean (-c)"
+	echo "	--fclean (-fc)"
+	echo "	--test (-t)"
+	echo "	--subtest (-st)"
 	echo "tests options :"
 	exit
 fi
@@ -58,15 +85,128 @@ then
 	exit
 fi
 
+get_opt "-c" "--clean"
+CLEAN="$?"
+
+get_opt "-fc" "--fclean"
+FCLEAN="$?"
+
 get_opt	"-v" "--verbose"
 VERBOSE="$?"
 
 get_opt "-s" "--stop"
 STOP="$?"
 
+get_opt "-t" "--test"
+FTEST="$?"
+
+get_opt "-st" "--subtest"
+SUBTEST="$?"
+
 ###############################################
-#################### TEST #####################
+################ FUNCTION TEST ################
 ###############################################
 
+function	test()
+{
+	if [ "$1" == "" ]
+	then
+		printf "$SCRIPT: line $BASH_LINENO: Function require the name of the test\n"
+		exit
+	fi
+	TEST="echo -n"
+	TEST_DIR=$CUR_LOGS_DIR/$1
 
+	if [ "$SUBTEST" == "1" ]
+	then
+		get_opt "$1"
+		if [ "$?" == "0" ]
+		then
+			return 0
+		fi
+	fi
 
+	RESEARCH=$($TIMEOUT $TEST)
+	RESULT=$($TIMEOUT $TEST)
+
+	if [ "$FCLEAN" == "1" ]
+	then
+		echo -n
+	elif [ "$CLEAN" == "1" ]
+	then
+		echo -n
+	fi
+
+	if [ "$VERBOSE" == "1" ]
+	then
+		echo -n
+	fi
+
+	if [ "$RESEARCH" != "$RESULT" ]
+	then
+		mkdir -p $TEST_DIR 
+		echo $RESULT > $TEST_DIR/$LOG_RESULT
+		echo $RESEARCH > $TEST_DIR/$LOG_RESEARCH
+		echo $TEST > $TEST_DIR/$LOG_TEST
+	
+		printf $BLUE"TEST\n"
+		printf $TEST$WHITE
+		printf $GREEN"\nRESEARCH\n"
+		printf $RESEARCH$WHITE
+		printf $RED"\nRESULT\n"
+		printf $RESULT$WHITE
+		if [ "$STOP" == "1" ]
+		then
+			exit
+		fi
+	else
+		printf $1$GREEN": Ok"$WHITE"\n"
+		
+		if [ "$VERBOSE" == "1" ]
+		then
+			mkdir -p $TEST_DIR
+			echo $RESULT > $TEST_DIR/$LOG_RESULT
+			echo $RESEARCH > $TEST_DIR/$LOG_RESEARCH
+			echo $TEST > $TEST_DIR/$LOG_TEST
+
+			printf $BLUE"TEST\n"
+			printf $TEST$WHITE
+			printf $GREEN"\nRESULT\n"
+			printf $RESULT$WHITE
+
+		fi
+	fi
+}
+
+function	is_called_test()
+{
+	if [ "$1" == "" ]
+	then
+		printf "$SCRIPT: line $BASH_LINENO: Function require the name of the test\n"
+		exit
+	fi
+
+	if [ "$FTEST" == "1" ]
+	then
+		get_opt "$1"
+		if [ "$?" == "1" ]
+		then
+			printf "***** TEST $1 *****\n"
+			return 1
+		fi
+		return 0
+	else
+		printf "TEST $1\n"
+		return 1
+	fi
+}
+
+###############################################
+##################### TEST ####################
+###############################################
+
+is_called_test "Vector"
+if [ "$?" == "1" ]
+then
+	test "a"
+fi
