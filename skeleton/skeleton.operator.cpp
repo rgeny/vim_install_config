2dd4f640d80213d36c7921f87072d09c46ddba1d
Remy GENY <rgeny@e1r5p1.clusters.42paris.fr>
2022-07-23 17:28:47 +0200

    add attribute((unused)) in skeleton.operator.cpp and debug make re -j for make_cpp

--- skeleton/skeleton.operator.cpp
+++ skeleton/skeleton.operator.cpp
@@ -1,13 +1,12 @@
 #include "skeleton.hpp"
 
-skeleton &	skeleton::operator=	(skeleton const & src)
+skeleton &	skeleton::operator=	(__attribute((unused)) skeleton const & src)
 {
 	return (*this);
 }
 
 std::ostream &	operator<<		(std::ostream & os
-								,skeleton const & src)
+								,__attribute((unused)) skeleton const & src)
 {
-	(void)src;
 	return (os);
 }
