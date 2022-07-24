75ce62407aed57af05dec31d1c7d29c80619348b
Remy GENY <rgeny@e1r7p1.clusters.42paris.fr>
2022-07-24 17:21:38 +0200

    del operator<< in skeleton.operator.cpp

--- skeleton/skeleton.operator.cpp
+++ skeleton/skeleton.operator.cpp
@@ -4,9 +4,3 @@ skeleton &	skeleton::operator=	(__attribute((unused)) skeleton const & src)
 {
 	return (*this);
 }
-
-std::ostream &	operator<<		(std::ostream & os
-								,__attribute((unused)) skeleton const & src)
-{
-	return (os);
-}
