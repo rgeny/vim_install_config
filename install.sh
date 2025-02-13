23a7c4ea761c8b824a3f71a083c76d164b67d2df
rgeny <remy.geny@gmail.com>
2025-02-13 20:45:11 +0100

    delete useless commentary in install.sh

--- install.sh
+++ install.sh
@@ -16,33 +16,3 @@ then
 	cp -r script/* ~/.script/
 fi
 
-#function add_cmd_zshrc()
-#{
-#	ZSHRC=~/.zshrc
-#	touch $ZSHRC
-#	ARG=$(cat $ZSHRC | grep "$*")
-#	if [ "$ARG" != "$*" ]
-#	then
-#		echo "$*" >> $ZSHRC
-#	fi
-#}
-#function add_cmd_bashrc()
-#{
-#	BASHRC=~/.bashrc
-#	touch $BASHRC
-#	ARG=$(cat $BASHRC | grep "$*")
-#	if [ "$ARG" != "$*" ]
-#	then
-#		echo "$*" >> $BASHRC
-#	fi
-#}
-#
-#NEWC="alias newc=\"mkdir includes srcs; touch Makefile srcs/main.cpp\""
-#add_cmd_zshrc "$NEWC"
-#add_cmd_bashrc "$NEWC"
-#
-#NEWCPPCLASS="
-#
-#HEADER="export USER=rgeny MAIL=rgeny@student.42.fr"
-#add_cmd_zshrc "$HEADER"
-#add_cmd_bashrc "$HEADER"
